7a07264e0d437ccbc716d68233555b3ce3f6782d
Oliver <wqdyxnbd@163.com>
2025-03-27 11:20:07 +0800

    不使用构建缓存

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 VERSION=${1:-v2}
-docker build  .  -t aolifu/ttsfm:$VERSION
+docker build --no-cache  .  -t aolifu/ttsfm:$VERSION
 docker push aolifu/ttsfm:$VERSION
 docker stop ttsfm
 docker rm ttsfm
